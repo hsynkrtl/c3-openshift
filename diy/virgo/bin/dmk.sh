95e331a2d34f8b645dbb986553e5b5c4e12b3576
Mikhail Malygin <mikhail.malygin@gmail.com>
2013-01-19 01:17:25 +0400

    change: update jetty ip script next attempt

--- diy/virgo/bin/dmk.sh
+++ diy/virgo/bin/dmk.sh
@@ -195,7 +195,7 @@ then
 			-Djava.security.auth.login.config=$AUTH_LOGIN \
 			-Dorg.eclipse.virgo.kernel.authentication.file=$AUTH_FILE \
 			-Djetty.home=$JETTY_HOME \
-			-Djetty.home=$OPENSHIFT_INTERNAL_IP \
+			-Djetty.host=$OPENSHIFT_INTERNAL_IP \
 			-Djetty.port=$OPENSHIFT_INTERNAL_PORT \
        		-Djava.io.tmpdir=$OPENSHIFT_TMP_DIR \
 			-Dorg.eclipse.virgo.kernel.home=$KERNEL_HOME \
